af33636c09e05e8b517aacb5f1907e82fe6b49d7
Ali Shamsian <Alishamsian504@gmail.com>
2021-12-19 23:26:25 +0330

    header final2

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -26,16 +26,16 @@ const Header = () => {
     const active = headerNav.findIndex(e => e.path === pathname);
 
     return (
-        <div ref={headerRef} className='header'>
-            <div className="header__warp container">
+        <div ref={headerRef} className="header">
+            <div className="header__wrap container">
                 <div className="logo">
                     <img src={logo} alt="" />
                     <Link to="/">tMovies</Link>
                 </div>
                 <ul className="header__nav">
                     {
-                        headerNav.map((e, i)=>(
-                            <li key={i} className={`${i === active ? 'active' : '' }`}>
+                        headerNav.map((e, i) => (
+                            <li key={i} className={`${i === active ? 'active' : ''}`}>
                                 <Link to={e.path}>
                                     {e.display}
                                 </Link>
@@ -45,7 +45,7 @@ const Header = () => {
                 </ul>
             </div>
         </div>
-    )
+    );
 }
 
 export default Header
